aa8560373be25285071b8af26ad40363b3a3b149
ParasArya289 <82272875+ParasArya289@users.noreply.github.com>
2023-11-23 18:28:33 +0000

    log

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ const userRouter = require("./Router/user.router.js")
 const app = express();
 app.use(express.json())
 app.use(cors());
-app.use("/user",userRouter);
+app.use("/user", userRouter);
 
-app.get("/data", (req, res) => {
-  res.status(200).json(data)
-})
 app.get("/", (req, res) => {
   res.send("Moonshot Data Visulization Server")
 })
-
-
+//Send Json data to client
+app.get("/data", (req, res) => {
+  res.status(200).json(data)
+})
+//Global Error Handler
 app.use((err, req, res, next) => {
   res.status(500).json({ error: "Something Went wrong!!" })
 })
 
+//Global Route Handler
 app.use((req, res) => {
   res.status(404).json({ error: "Requested API endpoint does not exist" })
 })
