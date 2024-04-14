eb516a7cfaf845df45f01b96ff4358609963d1c7
Paras Arya <82272875+ParasArya289@users.noreply.github.com>
2024-04-14 09:10:30 +0000

    update

--- Controllers/user.controllers.js
+++ Controllers/user.controllers.js
@@ -1,48 +1,62 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const {createAccount,findUserByUsername} = require("../Queries/user.queries.js");
+const {
+  createAccount,
+  findUserByUsername,
+} = require("../Queries/user.queries.js");
 
-const signupController = async(req,res)=>{
-  const {body} = req;
-  
-  try{ 
-  const {secret} = process.env;
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(body.password,salt);
-    if(!hashedPassword){
+const signupController = async (req, res) => {
+  const { body } = req;
+
+  try {
+    const { secret } = process.env;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(body.password, salt);
+    if (!hashedPassword) {
       throw new Error("Unable to hash password");
     }
 
-  const generatedToken = jwt.sign({username:body.username},secret); 
-  if(!generatedToken){
+    const generatedToken = jwt.sign({ username: body.username }, secret);
+    if (!generatedToken) {
       throw new Error("Unable to generate token");
     }
 
-    const savedUser = await createAccount({...body,password:hashedPassword});
-    res.status(201).json({message:"Account created",user:savedUser,token:generatedToken});
-  }catch(error){
-    res.status(500).json({message:"Failed to create User account"})
+    const savedUser = await createAccount({
+      ...body,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      message: "Account created",
+      user: savedUser,
+      token: generatedToken,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
-const loginController = async(req,res)=>{
-  const {username,password}=req.body;
-  const {secret} = process.env;
-  try{
-    if(!username || !password){
-      throw new Error("username and password fields are required")
+const loginController = async (req, res) => {
+  const { username, password } = req.body;
+  const { secret } = process.env;
+  try {
+    if (!username || !password) {
+      throw new Error("username and password fields are required");
     }
     const user = await findUserByUsername(username);
-    if(!user){
+    if (!user) {
       throw new Error("Incorrect username");
     }
-    const matchedPassword = await bcrypt.compare(password,user.password);
-    if(!matchedPassword){
+    const matchedPassword = await bcrypt.compare(password, user.password);
+    if (!matchedPassword) {
       throw new Error("Incorrect password");
     }
-    res.status(200).json({message:"successfully loggedin",user,token:jwt.sign({username},secret)})
-  }catch(error){
-    res.status(401).json({message:error.message})
+    res.status(200).json({
+      message: "successfully loggedin",
+      user,
+      token: jwt.sign({ username }, secret),
+    });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
   }
-}
-module.exports = {signupController,loginController};
+};
+module.exports = { signupController, loginController };
